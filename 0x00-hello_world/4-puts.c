d43470944400bab04172fe0ebd548aa5ce114745
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-04-20 15:57:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The beginning of programming in C
+ * Return: 0 (Return zero is a success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
