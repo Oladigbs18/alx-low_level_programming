8786a6f660fb45f628de60919c8776ca2541913e
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-04-20 21:45:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,6 @@
  * Return: 0 (success)
  */
 int main(void)
-{
-	write(l, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout)
+return (1);
 }
-
