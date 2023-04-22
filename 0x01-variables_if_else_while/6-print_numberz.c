615864254af3cac315ad8509f71294488a5d3b53
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-04-21 19:11:36 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - How to print numbers in c
+ * Description: 'to print all the numbers from 0 to 9'
+ * Return: 0 (Always 0)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
