3c879cafca2fd88109695daedcbe463723bd0602
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-04-21 19:04:21 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - How to input numbers in c
+ * Description: 'to print numbers from 0 to 9'
+ * Return: 0 (Always 0)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
