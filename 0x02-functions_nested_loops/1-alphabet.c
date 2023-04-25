547228da1d71e894375fc730e2edf74c2d7f2a76
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-04-25 15:26:03 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - 'function that prints alphabets in lowercase'
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
