8428bf650e966d480fa0e507d372a73403f95b48
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-04-25 17:45:25 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+nclude "main.h"
+/**
+ * add - Entry point
+ * @a: The first number
+ * @b: The second number
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+	_putchar('\n');
+}
