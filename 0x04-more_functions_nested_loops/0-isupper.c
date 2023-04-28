1dd5c5317aeb48ad4902040e1650df31daa4c36c
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-04-28 01:00:54 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - to print upperecase letters in c
+ * @c: This is a character in ASCII code
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c = 97, c <= 122, c++);
+	{
+		return (1);
+	else
+	{
+		return (0);
+	}
+	}
+}
