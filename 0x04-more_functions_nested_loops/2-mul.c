a88fa45cdb02e9503bc05253840350c9ececd982
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-04-28 09:10:34 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Check the code
+ * @a: This is an integer
+ * @b: This is scond integer
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
