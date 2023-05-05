9e53ba7ad53d3ff726dd3c89499d953b6b5ce46e
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-05-05 00:31:25 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -32,28 +32,28 @@ int main(void)
 		printf("Error\n");
 	}
 	else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "999999999";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        res = infinite_add(n, m, r3, 11);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        return (0);
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	n = "999999999";
+	m = "1";
+	res = infinite_add(n, m, r2, 10);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	res = infinite_add(n, m, r3, 11);
+	if (res == 0)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%s + %s = %s\n", n, m, res);
+	}
+	return (0);
 }
