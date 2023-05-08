b4613236dc407b8a987bc5df67a41bf173034b9e
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-05-08 08:38:49 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * *_memset - the first n bytes of the memory area pointed to by s with the constant byte b
+ * *_memset - the first n bytes of the memory area
+ * pointed to by s with the constant byte b
  * @s: the memory area pointed to
  * @b: the byte constant
  * @n: number of bytes
