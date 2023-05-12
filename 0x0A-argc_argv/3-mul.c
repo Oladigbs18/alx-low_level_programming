0b108bf0dba20f42a320f12bae5e4993e2ce3a42
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-05-12 08:44:21 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,16 +11,17 @@
 int main(int argc, char *argv[])
 {
 	int i, j, mul = 0;
-	if(argc <= 2)
+
+	if (argc <= 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	i = atoi(argv[1]);
 	j = atoi(argv[2]);
 	mul = i * j;
-	
+
 	printf("%d\n", mul);
 
 	return (0);
