2c6100097c936d3ed517aefc9fb35f641579c07c
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-05-12 08:33:01 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for AAliyah
+ * @argc: the number of command line arguments
+ * @argv: the array of command line arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int cents, change = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+		change++;
+	if ((cents - 25) >= 0)
+	{
+		cents -= 25;
+		continue;
+	}
+	if ((cents - 10) >= 0)
+	{
+		cents -= 10;
+		continue;
+	}
+	if ((cents - 5) >= 0)
+	{
+		cents -= 5;
+		continue;
+	}
+	if ((cents - 2) >= 0)
+	{
+		cents -= 2;
+		continue;
+	}
+	cents--;
+	}
+	printf("%d\n", change);
+	return (0);
+}
