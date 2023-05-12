6323ba0c18a2b166b69f518cec8478e1b1520ce0
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-05-12 07:12:21 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for AAliyah
+ * @argc: the number of command line arguments
+ * @argv: the array of command line arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
