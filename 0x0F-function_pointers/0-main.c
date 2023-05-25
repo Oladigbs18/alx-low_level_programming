22c3dffa325437c73d6c9c19f41e4ab35ffbe845
Oladigbs18 <aaliyaholadigbolu@gmail.com>
2023-05-25 05:04:42 -0700

    0-print name.c

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -1,9 +1,5 @@
-#include <stdio.h>
 #include "function_pointers.h"
-
-void print_name_uppercase(char *name);
-void print_name_as_is(char *name);
-int main(void);
+#include <stdio.h>
 
 /**
  * print_name_as_is - prints a name as is
