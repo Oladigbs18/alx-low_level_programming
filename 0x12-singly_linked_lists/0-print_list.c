17ad6f482be79b85957bc3c1977f25465dc29308
Aaliyah <aaliyaholadigbolu@gmail.com>
2023-08-24 23:15:53 +0100

    linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%lu] %s\n", node_count, h->str);
+		}
+
+		h = h->next;
+		node_count++;
+	}
+
+	return node_count;
+}
