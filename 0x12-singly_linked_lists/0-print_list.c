e53365a43ddec3bf157d1ccc5c45aedae548fe18
Aaliyah <aaliyaholadigbolu@gmail.com>
2023-09-01 01:48:40 +0100

    linked list updated😍😍✔✔

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,23 @@
-#include <stddef.h>
-
-size_t node_count = 0; 
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
 
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 0;
+	size_t nelem;
 
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-		{
-			printf("[%lu] %s\n", node_count, h->str);
-		}
-
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		node_count++;
+		nelem++;
 	}
-
-	return node_count;
+	return (nelem);
 }
